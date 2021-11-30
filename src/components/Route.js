92dc01ee856d1ec22cdeb16912cb5e0492176b05
Your Namef <obiobialo@aol.com>
2021-11-30 16:31:32 -0600

    widgets app

--- src/components/Route.js
+++ src/components/Route.js
@@ -1,8 +1,23 @@
 // No need to import React from "react"
 // because there is no JSX in file
+import { useEffect, useState } from "react";
 
 const Route = ({ path, children }) => {
-    return window.location.pathname === path ? children : null;
+    const [currentPath, setCurrentPath] = useState(window.location.pathname);
+
+    useEffect(() => {
+        const onLocationChange = () => {
+            setCurrentPath(window.location.pathname);
+        }
+
+        window.addEventListener("popstate", onLocationChange);
+
+        return () => {
+            window.removeEventListener("popstate", onLocationChange);
+        };
+    }, [])
+    
+    return currentPath === path ? children : null;
 };
 
 export default Route;
